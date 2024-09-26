b6554468efabdeefe66fc59c71cd466e31c6eb63
MiriamS-byte <schataram161200@gmail.com>
2024-09-26 18:43:10 +0300

    ADD VAR AND ONST

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello World"); //afisare, ca si println
+
+//variabila declarata si apoi valoarea atribuita
+let userName;
+userName = "Ion";
+console.log(userName);
+// reatribuire alta valoare
+userName = "_Maria"
+console.log(userName);
+
+//variabila declarata si valoare si initiata cu o valoare in aceeasi linie
+let email = "user@desktop.com";
+console.log(email);
+
+// declarare constante - trebuie initializate din prima
+// nu se poate modifica valoarea
+const piNumber = 3.1415;
+console.log(piNumber);
+// piNumber = 15; - INTERZISA REATRIBUIREA
+
